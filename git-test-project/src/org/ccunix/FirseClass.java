0600875c8690d5768fc6c2ef92942a32ac3aed6b
bobo <syswjb@126.com>
2021-01-01 21:26:15 +0800

    初始化版本

---
+++ git-test-project/src/org/ccunix/FirseClass.java
@@ -0,0 +1,11 @@
+package org.ccunix;
+
+public class FirseClass {
+    public static void main(String[] args) {
+        System.out.println("测试 11111111111111");
+        System.out.println("测试 11111111111111");
+        System.out.println("测试 11111111111111");
+        System.out.println("测试 11111111111111");
+        System.out.println("测试 11111111111111");
+    }
+}
