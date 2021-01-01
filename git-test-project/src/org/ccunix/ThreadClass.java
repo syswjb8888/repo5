7b4398fd5b3780da3edac1de9baa50fbc0cda967
bobo <syswjb@126.com>
2021-01-01 21:48:21 +0800

    thread类型初始化

---
+++ git-test-project/src/org/ccunix/ThreadClass.java
@@ -0,0 +1,13 @@
+package org.ccunix;
+
+public class ThreadClass {
+    public static void main(String[] args) {
+        System.out.println("thread 333333333333333");
+        System.out.println("thread 333333333333333");
+        System.out.println("thread 333333333333333");
+        System.out.println("thread 333333333333333");
+        System.out.println("thread 333333333333333");
+        System.out.println("thread 333333333333333");
+        System.out.println("thread 333333333333333");
+    }
+}
