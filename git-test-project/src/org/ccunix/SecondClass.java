5a19a4c8787e4a943760eb8c0ddab1a932443b54
bobo <syswjb@126.com>
2021-01-01 21:29:53 +0800

    第二个测试类

---
+++ git-test-project/src/org/ccunix/SecondClass.java
@@ -0,0 +1,11 @@
+package org.ccunix;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("第二个测试类 11111111111111");
+        System.out.println("第二个测试类 11111111111111");
+        System.out.println("第二个测试类 11111111111111");
+        System.out.println("第二个测试类 11111111111111");
+        System.out.println("第二个测试类 11111111111111");
+    }
+}
